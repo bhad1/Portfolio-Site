364e2b681aa11acc12fc17712f43e897ac26be8a
bhadaway <bhadaway@bridge2solutions.com>
2017-04-11 16:16:04 -0400

    finished the carousel

--- src/app/portfolio/projects/angular-carousel/angular-carousel.component.ts
+++ src/app/portfolio/projects/angular-carousel/angular-carousel.component.ts
@@ -4,8 +4,8 @@ import {Image} from './image';
 const IMAGES: Image[] = [
   {"name": "First", "url": "https://placekitten.com/g/2000/1500"},
   {"name": "Second", "url": "https://placekitten.com/g/3000/2000"},
-  {"name": "Third", "url": "https://placekitten.com/g/2000/1500"},
-  {"name": "Fourth", "url": "https://placekitten.com/g/3000/2000"}
+  {"name": "Third", "url": "https://placekitten.com/g/1500/1000"},
+  {"name": "Fourth", "url": "https://placekitten.com/g/6000/4000"}
 ];
 
 @Component({
@@ -17,6 +17,51 @@ const IMAGES: Image[] = [
 export class AngularCarouselComponent implements OnInit, OnChanges {
   @Input() showProjectNumber: number;
   showProject;
+  showImage = 0;
+
+  nextImage(){
+    if(this.showImage === this.images.length-1){
+      this.showImage = 0;
+    } else{
+      this.showImage ++;
+    }
+
+  }
+
+  prevImage(){
+    if(this.showImage === 0){
+      this.showImage = 3;
+    }else {
+      this.showImage --;
+    }
+  }
+
+  fillCircleOne(){
+    if(this.showImage === 0){
+      return 'black';
+    }
+  }
+  fillCircleTwo(){
+    if(this.showImage === 1){
+      return 'black';
+    }
+  }
+  fillCircleThree(){
+    if(this.showImage === 2){
+      return 'black';
+    }
+  }
+  fillCircleFour(){
+    if(this.showImage === 3){
+      return 'black';
+    }
+  }
+
+  setImageIndicators(imgNum: number){
+    this.showImage = imgNum;
+  }
+
+
 
   public images = IMAGES;
 
@@ -26,8 +71,18 @@ export class AngularCarouselComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     setInterval(() => {
-      IMAGES.push(IMAGES.shift());
+      this.showImage ++;
+      if(this.showImage === this.images.length){
+        this.showImage = 0;
+      }
+      // if(this.showImage === this.images.length-1){
+      //   this.showImage = 0;
+      // }else{
+      //   this.showImage ++;
+      // }
+      console.log(this.showImage);
     }, 5000);
+
   }
 
   ngOnChanges() {
@@ -36,6 +91,7 @@ export class AngularCarouselComponent implements OnInit, OnChanges {
     }else {
       this.showProject = false;
     }
+
   }
 
 }
